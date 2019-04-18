38c47c6f3b2f22e93b408312ddf8583a3504a28b
Ariel Hicks <hicks.ariel@gmail.com>
2019-04-18 15:16:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get 'name' do
+  get '/name' do
     "My name is Ariel."
   end
 
-  get 'hometown' do
+  get '/hometown' do
     "My hometown is Alba, Missouri."
   end
 
-  get 'favorite-song' do
+  get '/favorite-song' do 
     "My favorite song is Karma."
 end
